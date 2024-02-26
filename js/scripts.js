fe179a415ec85357061104b5a28935c303b9ccff
MaGloCon <mgcontrada@gmail.com>
2024-02-26 18:48:04 +0100

    Refactored 'for' loop to forEach() loop

--- js/scripts.js
+++ js/scripts.js
@@ -14,16 +14,16 @@ let pokemonList = [
     {name: 'Clefable', height: 1.3, type: ['fairy']}
 ];
 
-// print Pokemon names and corresponding heights from pokemonList
-for (let i=0; i < pokemonList.length; i++) {
-    //print name and height with message if the height is less than to 0.6 m
-    //print name and height with message if the height is more than to 1.2 m
-    //otherwise print only name and height
-    if (pokemonList[i].height < 0.6) { 
-        document.write('<p>', pokemonList[i].name + ' (height: ' + pokemonList[i].height + ' m)' + ' - Small, but mighty! </p>');
-    } else if (pokemonList[i].height > 1.2) {
-        document.write('<p>', pokemonList[i].name + ' (height: ' + pokemonList[i].height + ' m)' + ' - Wow, that\'s a big one! </p>');
+/* print Pokemon names and corresponding heights from pokemonList:
+   // print name and height with message if the height is less than to 0.6 m
+   // print name and height with message if the height is more than to 1.2 m
+   // otherwise print only name and height */
+pokemonList.forEach(function(pokemon) {
+    if (pokemon.height < 0.6) { 
+        document.write('<p>' + pokemon.name + ' (height: ' + pokemon.height + ' m)' + ' - Small, but mighty! </p>');
+    } else if (pokemon.height > 1.2) {
+        document.write('<p>' + pokemon.name + ' (height: ' + pokemon.height + ' m)' + ' - Wow, that\'s a big one! </p>');
     } else {
-        document.write('<p>', pokemonList[i].name + ' (height: ' + pokemonList[i].height + ' m)' + ' </p>');
+        document.write('<p>' + pokemon.name + ' (height: ' + pokemon.height + ' m)' + ' </p>');
     }
-}
+});
