3801a55a54c6e697883b1f3c91e1da22491e354f
MaGloCon <mgcontrada@gmail.com>
2024-03-04 00:21:34 +0100

    Add functions to load data from Pokemon API
    scripts.js: modified add() function to extend parameters and explain error. Modified addListItem() function. Added loadList(), loadDetails() and show details functions to load Pokemon data from API. Added functions to display loading message while data is being loaded

--- js/scripts.js
+++ js/scripts.js
@@ -69,59 +69,94 @@ let pokemonRepository = (function () {
     ];
 
     function add(pokemon) {
-        if (typeof pokemon === 'object') {
+        if (
+            typeof pokemon === 'object' &&
+            'name' in pokemon &&
+            'detailsURL' in pokemon
+        ) {
             pokemonList.push(pokemon);
-     }
+        } else {
+            console.log('Pokemon is not correct');
+        }
+    }
+
+    function addListItem(pokemon) {
+       let pokemonList = document.querySelector('.pokemon-list');
+       let listPokemon = document.createElement('li');
+       let button = document.createElement('button');
+       button.innerText = pokemon.name;
+       button.classList.add('button-class');
+       listPokemon.appendChild(button);
+       pokemonList.appendChild(listPokemon);
+       button.addEventListener('click', function(event) {
+           showDetails(pokemon);
+       });
+    }
+
+
+    function loadList() {
+        showLoadingMessage();
+        return fetch(apiUrl).then(function(response) {
+            return response.json();
+        }).then(function(json) {
+            hideLoadingMessage();
+            json.results.forEach(function(item) {
+                let pokemon = {
+                    name: item.name,
+                    detailsURL: item.url
+                };
+                add(pokemon);
+            });
+        }).catch(function (e) {
+            hideLoadingMessage();
+            console.error(e);
+        });
+    }
+
+    function loadDetails(item) {
+        let url = item.detailsUrl;
+        return fetch(url).then(function (response) {
+            return response.json();
+        }).then(function (details) {
+            // adds details to item
+            item.imgUrl = details.sprites.front_default; //change to home front_default later
+            item.height = details.height;
+            item.types = details.types; // add a for loop to iterate trough each type!!!
+        }).catch(function (e) {
+            console.error(e);
+        });
+    }
+
+    function showDetails(pokemon) {
+        loadDetails(pokemon).then(function(){
+            console.log(pokemon);
+        }); 
     }
 
     function getAll() {
         return pokemonList;
     }
 
-    function filterByName(name) {
-        return pokemonList.filter(function(pokemon) {
-            return pokemon.name === name;
-        });
-    }
-
-    function addListItem(pokemon) {
-        let pokemonList = document.querySelector('.pokemon-list');
-        let li= document.createElement('li');
-        pokemonList.appendChild(li);
-        li.classList.add('pokemon-name-list');
-
-        let button = document.createElement('button');
-        button.innerText = pokemon.name;
-        li.appendChild(button);
-
-        button.classList.add('pokemon-name-button');
-
-        addEventListener(button, pokemon);
-    }
-
-    function addEventListener(button, pokemon) {
-        button.addEventListener('click', function() {
-            showDetails(pokemon);
-        });
-
-        function showDetails(pokemon) {
-            console.log(pokemon);
-        }
-    }
     return {
         add: add,
         getAll: getAll,
         addListItem: addListItem,
-        filterByName: filterByName
+        loadList: loadList,
+        loadDetails: loadDetails,
+        showDetails: showDetails
     }
 }) ();
 
-pokemonRepository.add({name: 'Arcanine', height: 1.9, type: ['fire']});
-
-// print Pokemon names and corresponding heights from pokemonList:
-pokemonRepository.getAll().forEach(function(pokemon) {
-   pokemonRepository.addListItem(pokemon);
+pokemonRepository.loadList().then(function() {
+    pokemonRepository.getAll().forEach(function(pokemon) {
+        pokemonRepository.addListItem(pokemon);
+    });
 });
 
-let filterByName = pokemonRepository.filterByName('Bulbasaur');
-console.log(filterByNameResult[0]);
+function showLoadingMessage() {
+    document.getElementById('loadingMessage');
+}
+
+function hideLoadingMessage() {
+    document.getElementById('loadingMessage');
+}
