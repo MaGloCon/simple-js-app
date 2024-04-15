736920230716e6a93589f22219c1977e7033084f
MaGloCon <mgcontrada@gmail.com>
2024-04-15 19:42:52 +0200

    Added metric/imperial toggle switch, Added code to change back-ground-color of buttons based on Pokémon type
    - Added Pokéball.svg and added it to navbar
    - Added toggle switch to convert height and weight between metric and imperial units in modal
    - Added code to change background-color of Pokémon buttons based on first Pokémon type in type array. If first type was normal, then second type would be taken

--- js/scripts.js
+++ js/scripts.js
@@ -31,11 +31,15 @@ let pokemonRepository = (function () {
                 return fetch(item.url).then(function(response) {
                     return response.json();
                 }).then(function(details) {
-                    let pokemon = {//create a new object with the Pokemon's details
+                    let pokemon = { //create a new object with the Pokemon's details
                         name: item.name,
                         detailsURL: item.url,
                         imageUrl: details.sprites.other.dream_world.front_default,
+                        type: details.types[0].type.name, //for button color; set the type to the first type 
                     };
+                    if (details.types.length > 1 && pokemon.type === 'normal') { //for button color: to set the type to the second type if the first type is normal
+                        pokemon.type = details.types[1].type.name;
+                    }
                     add(pokemon); 
                 });
             });
@@ -51,6 +55,7 @@ let pokemonRepository = (function () {
        // Create a button element 
        let button = document.createElement('button');
        button.classList.add('pokemon-button');
+       button.classList.add(pokemon.type);
 
        // Create an img element and set its src attribute to the Pokemon's image URL
         let img = document.createElement('img');
@@ -71,6 +76,7 @@ let pokemonRepository = (function () {
            showDetails(pokemon);
        });
     }
+    
 
     // Load the Pokemon details from the API
     function loadDetails(item) {
@@ -79,7 +85,8 @@ let pokemonRepository = (function () {
             return response.json();
         }).then(function (details) {
             item.imgURL = details.sprites.other.dream_world.front_default; 
-            item.height = details.height;
+            item.height = details.height / 10; //convert height (decimeters) to meters
+            item.weight = details.weight / 10; //convert weight (hectograms) to kilograms
             item.types = details.types;
             item.abilities = details.abilities;
         }).catch(function (e) {
@@ -104,11 +111,11 @@ let pokemonRepository = (function () {
         //Create a new modal
         let modal = document.createElement('div');
         modal.classList.add('modal');
-
+        
         //Add new modal content
         let closeButtonElement = document.createElement('button');
         closeButtonElement.classList.add('modal-close');
-        closeButtonElement.innerText = 'Close';
+        closeButtonElement.innerText = 'X';
         closeButtonElement.addEventListener('click', hideModal);
 
         let titleElement = document.createElement('h1');
@@ -120,19 +127,64 @@ let pokemonRepository = (function () {
         let heightElement = document.createElement('p');
         heightElement.innerText = 'Height: ' + item.height + ' m';
 
+        let weightElement = document.createElement('p');
+        weightElement.innerText = 'Weight: ' + item.weight + ' kg';
+        
         let typesElement = document.createElement('p');
         typesElement.innerText = 'Types: ' + item.types.map((type) => type.type.name.charAt(0).toUpperCase() + type.type.name.slice(1)).join(', ');
         
         let abilitiesElement = document.createElement('p');
         abilitiesElement.innerText = 'Abilities: ' + item.abilities.map((ability) => ability.ability.name.charAt(0).toUpperCase() + ability.ability.name.slice(1)).join(', ');
+        
+        //Toggle switch to convert height and weight between metric and imperial units
+        let toggleSwitch = document.createElement('label');
+        toggleSwitch.classList.add('switch');
 
+        let inputElement = document.createElement('input');
+        inputElement.setAttribute('type', 'checkbox');
+        inputElement.addEventListener('change', function() {
+            if (this.checked) {
+                let [height, weight] = toImperial(item.height, item.weight);
+                heightElement.innerText = 'Height: ' + height + ' ft';
+                weightElement.innerText = 'Weight: ' + weight + ' lbs';
+            } else {
+                let [height, weight] = toMetric(item.height, item.weight);
+                heightElement.innerText = 'Height: ' + height + ' m';
+                weightElement.innerText = 'Weight: ' + weight + ' kg';
+            }
+        });
+
+        let sliderElement = document.createElement('span');
+        sliderElement.classList.add('slider', 'round');
+
+        let imperialElement = document.createElement('span');
+        imperialElement.innerText = 'Imperial';
+        imperialElement.style.float = 'left';
+        
+        let metricElement = document.createElement('span');
+        metricElement.innerText = 'Metric';
+        metricElement.style.float = 'right';
+
+        sliderElement.appendChild(imperialElement);
+        sliderElement.appendChild(metricElement);
+
+        toggleSwitch.appendChild(inputElement);
+        toggleSwitch.appendChild(sliderElement);
+        toggleSwitch.appendChild(imperialElement);
+        toggleSwitch.appendChild(inputElement);
+        toggleSwitch.appendChild(sliderElement);
+        toggleSwitch.appendChild(metricElement);
+        
         //Append modal content to the modal
         modal.appendChild(closeButtonElement);
-        modal.appendChild(titleElement);
         modal.appendChild(imgElement);
+        modal.appendChild(titleElement);
         modal.appendChild(heightElement);
+        modal.appendChild(weightElement);
         modal.appendChild(typesElement);
         modal.appendChild(abilitiesElement);
+        modal.appendChild(toggleSwitch);
+
         modalContainer.appendChild(modal);
 
         //Add the modal to the page
@@ -161,6 +213,19 @@ let pokemonRepository = (function () {
         }
     });
 
+    function toImperial(height, weight) {
+        // Convert height from meters to feet and weight from kilograms to pounds
+        let heightInFeet = height * 3.28084;
+        let weightInPounds = weight * 2.20462;
+        return [heightInFeet.toFixed(2), weightInPounds.toFixed(2)];
+    }
+    
+    function toMetric(height, weight) {
+        // Convert height from feet to meters and weight from pounds to kilograms
+        let heightInMeters = height / 3.28084;
+        let weightInKilograms = weight / 2.20462;
+        return [heightInMeters.toFixed(2), weightInKilograms.toFixed(2)];
+    }
     // Search bar to filter Pokemon list by name
     let searchBar = document.querySelector('#search-bar');
 
