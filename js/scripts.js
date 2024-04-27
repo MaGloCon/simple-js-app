f64a37204968371b73fc149012c47ff9911a6e09
MaGloCon <mgcontrada@gmail.com>
2024-04-27 14:37:04 +0200

    Linted code, added minified files to dis folder
    - Linted the JS file with ESLint.
    - Added minified CSS and JS files to the dist folder
    - Updated links in HTML.

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,12 @@ let pokemonRepository = (function () {
     let apiUrl = 'https://pokeapi.co/api/v2/pokemon/';
     let offset = 0; // Start at the beginning
     let limit = 50; // Fetch 50 Pokemon at a time
-    
+
     // Capitalize the first letter of a string
-    function capitalizeFirstLetter(string) { 
+    function capitalizeFirstLetter(string) {
         return string.charAt(0).toUpperCase() + string.slice(1);
     }
 
-    let unusedVariable = 
     // Add Pokemon to the list
     function add(pokemon) {
         if (
@@ -53,7 +52,7 @@ let pokemonRepository = (function () {
     }
 
     // Load the Pokemon details from the API -- 
-    async function loadDetails(item) { 
+    async function loadDetails(item) {
         let url = item.detailsURL;
         try {
             const response = await fetch(url);
@@ -70,51 +69,51 @@ let pokemonRepository = (function () {
 
     // Show the Pokemon's details in the modal
     function showDetails(pokemon) {
-        loadDetails(pokemon).then(function(){
+        loadDetails(pokemon).then(function () {
             showModal(pokemon);
-        }); 
+        });
     }
 
     // Add Pokemon to the list as a button
     function addListItem(pokemon) {
         // Create a list item element
         let listPokemon = $('<div>').addClass('pokemon-div col-9 col-md-4 col-lg-3 border m-1 rounded-lg ' + pokemon.type);
-    
+
         // Create a button element 
         let button = $('<button>').addClass('pokemon-button btn d-flex justify-content-between align-items-center')
-                                  .attr('data-toggle', 'modal')
-                                  .attr('data-target', '#pokemonModal');
-    
+            .attr('data-toggle', 'modal')
+            .attr('data-target', '#pokemonModal');
+
         // Create an image element
         let image = $('<img>').attr('src', pokemon.imageUrl)
-                              .attr('alt', `Image of ${pokemon.name}`)
-                              .addClass('pokemon-image img-fluid');
+            .attr('alt', `Image of ${pokemon.name}`)
+            .addClass('pokemon-image img-fluid');
         button.append(image);
-    
+
         // Create textNode for pokemon name; span element for styling 
         let span = $('<span>').addClass('text-white').text(capitalizeFirstLetter(pokemon.name));
         button.append(span);
-    
+
         // Append the button to the list item and the list item to the Pokemon list
         listPokemon.append(button);
         $('.row').append(listPokemon)
-                 .addClass('justify-content-center');
-        
+            .addClass('justify-content-center');
+
         // Add a delay in showing the Pokemon list for smoother loading and better user experience
-        setTimeout(() => listPokemon.addClass('visible'), 100);
-        
+        setTimeout(() => listPokemon.addClass('visible'), 50);
+
         // Show Pokemon's details when Pokemon button is clicked
-        button.click(function() {
+        button.click(function () {
             showDetails(pokemon);
         });
     }
 
     // Show the modal with the Pokemon's details
     function showModal(item) {
-        $('.image-background').attr('class', 'image-background').addClass(item.type); 
-    
+        $('.image-background').attr('class', 'image-background').addClass(item.type);
+
         // Set the modal elements
-        $('#pokemonModalTitle').text(capitalizeFirstLetter(item.name));        
+        $('#pokemonModalTitle').text(capitalizeFirstLetter(item.name));
         $('#pokemonModalImage').attr('src', item.imgURL).addClass('modal-image img-fluid');
         $('#pokemonModalHeight').text(`Height: ${item.height} m`).addClass('pt-3');
         $('#pokemonModalWeight').text(`Weight: ${item.weight} kg`);
@@ -126,9 +125,9 @@ let pokemonRepository = (function () {
     function searchBar() {
         let $searchBar = $('#search-bar');
 
-        $searchBar.on('input', function() {
-            let searchValue = $searchBar.val().toLowerCase(); 
-            let filteredPokemon = pokemonList.filter(pokemon => pokemon.name.toLowerCase().startsWith(searchValue)); 
+        $searchBar.on('input', function () {
+            let searchValue = $searchBar.val().toLowerCase();
+            let filteredPokemon = pokemonList.filter(pokemon => pokemon.name.toLowerCase().startsWith(searchValue));
 
             // Clear the Pokemon list
             let $pokemonListElement = $('.row');
@@ -145,45 +144,44 @@ let pokemonRepository = (function () {
             }
         });
     }
-    searchBar(); 
+    searchBar();
 
     // Search icon and searchBar toggle
     let searchIcon = document.querySelector('#search-icon');
     let searchBarContainer = document.querySelector('#search-bar-container');
-    searchIcon.addEventListener('click', function() {
+    searchIcon.addEventListener('click', function () {
         searchBarContainer.classList.toggle('d-none');
     });
 
     // Load more Pokemon when the user scrolls to the bottom of the page
-    $(window).on('scroll', function() {
+    $(window).on('scroll', function () {
         if ($(window).innerHeight() + $(window).scrollTop() >= $(document).height()) { // If the user has scrolled to the bottom of the page
             offset += limit;  // Increase the offset by the limit
-            loadList().then(function() { //
-                pokemonRepository.getAll().slice(-limit).forEach(function(pokemon) { 
+            loadList().then(function () { //
+                pokemonRepository.getAll().slice(-limit).forEach(function (pokemon) {
                     pokemonRepository.addListItem(pokemon); //
                 });
             });
         }
     });
 
-    
     // Scroll to top button
     let $mybutton = $("#scrollToTopButton");
 
     // When the user scrolls down 20px from the top of the document, show the button
-    $(window).scroll(function() {
+    $(window).scroll(function () {
         if ($(window).scrollTop() > 20) {
             $mybutton.show();
         } else {
             $mybutton.hide();
         }
     });
-
+    
     // When the user clicks on the button, scroll to the top of the document
-    $mybutton.click(function() {
-        $('html, body').animate({scrollTop: 0}, 'slow');
+    $mybutton.click(function () {
+        $('html, body').animate({ scrollTop: 0 }, 'slow');
     });
-        
+
     return {
         add: add,
         getAll: getAll,
@@ -192,12 +190,11 @@ let pokemonRepository = (function () {
         loadDetails: loadDetails,
         showDetails: showDetails,
     }
-}) ();
+})();
 
 //Fetch and Load Pokemon list
-pokemonRepository.loadList().then(function() {
-    pokemonRepository.getAll().forEach(function(pokemon) {
+pokemonRepository.loadList().then(function () {
+    pokemonRepository.getAll().forEach(function (pokemon) {
         pokemonRepository.addListItem(pokemon);
     });
 });
-
